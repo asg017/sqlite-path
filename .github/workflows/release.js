fd9f63c4c3630130bff8628acfbbe59e4484fd1b
Alex Garcia <alexsebastian.garcia@gmail.com>
2022-08-29 12:08:06 -0700

    update release for zig cross

--- .github/workflows/release.js
+++ .github/workflows/release.js
@@ -25,11 +25,40 @@ module.exports = async ({ github, context }) => {
     });
   }
   await Promise.all([
-    uploadReleaseAsset("path0.so", "path0-linux-amd64/path0.so"),
-    uploadReleaseAsset("path0.dylib", "path0-darwin-amd64/path0.dylib"),
-    uploadReleaseAsset("path0.dll", "path0-windows-amd64/path0.dll"),
     uploadReleaseAsset("path0-sqljs.wasm", "path0-sqljs/sqljs.wasm"),
     uploadReleaseAsset("path0-sqljs.js", "path0-sqljs/sqljs.js"),
+    uploadReleaseAsset(
+      "linux-x86_64-gnu-path0.so",
+      "path0-cross/x86_64-linux-gnu/path0.so"
+    ),
+    uploadReleaseAsset(
+      "linux-x86_64-path0.so",
+      "path0-cross/x86_64-linux/path0.so"
+    ),
+    uploadReleaseAsset(
+      "linux-i386-path0.so",
+      "path0-cross/i386-linux/path0.so"
+    ),
+    uploadReleaseAsset(
+      "linux-aarch64-path0.so",
+      "path0-cross/aarch64-linux/path0.so"
+    ),
+    uploadReleaseAsset(
+      "macos-x86_64-path0.dylib",
+      "path0-cross/x86_64-macos/path0.dylib"
+    ),
+    uploadReleaseAsset(
+      "macos-aarch64-path0.dylib",
+      "path0-cross/aarch64-macos/path0.dylib"
+    ),
+    uploadReleaseAsset(
+      "windows-i3860-path0.dll",
+      "path0-cross/i386-windows/path0.dll"
+    ),
+    uploadReleaseAsset(
+      "windows-x86_64-path0.dll",
+      "path0-cross/x86_64-windows/path0.dll"
+    ),
   ]);
 
   return;
